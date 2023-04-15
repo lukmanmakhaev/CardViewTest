aab260d9433eae45819005354421256a64f6d704
Lukman <lukmanmakhaev@gmail.com>
2023-04-15 16:13:12 +0300

    Initial Commit

---
+++ CardViewTest/CardViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewTestApp.swift
+//  CardViewTest
+//
+//  Created by Lukman Makhaev on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
